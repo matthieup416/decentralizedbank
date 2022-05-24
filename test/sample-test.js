1cb7d1ecddfb386dc1af97d88f1f1a20425ef4b5
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2022-05-24 13:44:40 +0200

    updated tests chai

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,18 +3,23 @@ const { ethers } = require('hardhat')
 
 describe('Bank', async function () {
   it('Should deposit 10ETH in bank', async function () {
-    // const Token = await ethers.getContractFactory("Token");
-    // const hardhatToken = await Token.deploy();
-    // const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    // expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
-    ;[owner, addr1] = await ethers.getSigners()
-    console.log('owner.address', owner.address)
+    ;[owner] = await ethers.getSigners()
     const Bank = await ethers.getContractFactory('Bank')
     const bank = await Bank.deploy()
     await bank.deployed()
-
+    console.log('owner.address', owner.address)
     await bank.deposit({ value: ethers.utils.parseEther('10') })
-    // const assets = await bank.fetchPosts()
+    expect(await bank.checkAssets()).to.equal(ethers.utils.parseEther('10'))
+  })
+
+  it('Should add 20 ETH and withdraw 10ETH in bank', async function () {
+    ;[owner] = await ethers.getSigners()
+    const Bank = await ethers.getContractFactory('Bank')
+    const bank = await Bank.deploy()
+    await bank.deployed()
+    await bank.deposit({ value: ethers.utils.parseEther('20') })
+    console.log('owner.address', owner.address)
+    await bank.withdraw(ethers.utils.parseEther('10'))
     expect(await bank.checkAssets()).to.equal(ethers.utils.parseEther('10'))
   })
 })
